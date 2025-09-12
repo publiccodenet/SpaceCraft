a74395da7b87cf9b97c3fda411dcd8ae44a39912
Don Hopkins <dhopkins@DonHopkins.com>
2025-09-12 04:43:55 +0200

    now supports multiple simulators, controller navigation tab has simulator menu, qr code urls set simulator to correspond with each simulator, user can switch controller's simulator with menu, menu updates when new simulators attach and leave.

--- WebSites/controller/build/TabNavigate.js
+++ WebSites/controller/build/TabNavigate.js
@@ -4,7 +4,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-import { h2, Register } from 'io-gui';
+import { h2, Register, IoOptionSelect, MenuOption, br } from 'io-gui';
 import { TabBase } from './TabBase.js';
 let TabNavigate = class TabNavigate extends TabBase {
     static get Style() {
@@ -28,9 +28,47 @@ let TabNavigate = class TabNavigate extends TabBase {
         }
     }
     changed() {
+        // Force rerender when simulator list changes by reading controller.simulatorRosterTick
+        void this.controller.simulatorRosterTick;
+        const simulators = Array.from(this.controller.currentSimulators?.values() || []);
+        const hasSimulators = simulators.length > 0;
+        const headerRow = hasSimulators
+            ? [
+                h2('Simulator:'),
+                br(),
+                IoOptionSelect.vConstructor({
+                    value: this.controller.currentSimulatorId || '',
+                    option: new MenuOption({
+                        options: simulators
+                            .map(s => ({ id: (s.clientName || s.clientId), value: s.clientId }))
+                            .sort((a, b) => a.id.localeCompare(b.id, undefined, { sensitivity: 'base' }))
+                    }),
+                    '@value-input': (e) => this.onSimulatorChange(e)
+                })
+            ]
+            : [
+                h2('Simulator:'),
+                br(),
+                IoOptionSelect.vConstructor({
+                    value: '(none)',
+                    option: new MenuOption({
+                        options: [{ id: '(none)', value: '(none)' }],
+                        disabled: true,
+                    }),
+                    // no handler; locked selector
+                    'disabled': true
+                })
+            ];
         this.render([
-            h2('DRAG to pan • SCROLL to zoom'),
+            ...headerRow,
+            h2('DRAG to pan • PINCH to zoom'),
         ]);
+    }
+    onSimulatorChange(event) {
+        const newId = event.detail?.value;
+        if (newId && newId !== this.controller.currentSimulatorId && newId !== '(none)') {
+            this.controller.setCurrentSimulator?.(newId);
+        }
     }
 };
 TabNavigate = __decorate([
